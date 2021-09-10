b82de10f2a1edb6e535c1a20073266d585649eda
Leonardo Nascimento <Developerheart>
2021-09-10 09:39:23 -0400

    Base do projeto

---
+++ src/main/java/com/br/tec/apigerenciacerveja/ApiGerenciaCervejaApplication.java
@@ -0,0 +1,13 @@
+package com.br.tec.apigerenciacerveja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciaCervejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciaCervejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/tec/apigerenciacerveja/ApiGerenciaCervejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.tec.apigerenciacerveja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciaCervejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
